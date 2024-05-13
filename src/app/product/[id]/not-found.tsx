ebf318d8bf8f044da345edabc4414897f9f625ae
garg624 <gargayush970@gmail.com>
2024-05-13 19:41:51 +0530

    router error from notfound page is solved

--- src/app/product/[id]/not-found.tsx
+++ src/app/product/[id]/not-found.tsx
@@ -1,13 +1,11 @@
-"use client"
-import React from 'react'
-import  { useRouter } from "next/navigation"
+
+import Link from "next/link"
+
 
 const NotFound = () => {
-  const router=useRouter();
+
   
-  const goBack = () => {
-    router.back();
-  }
+  
   return (
     <div className="flex items-center justify-center h-screen bg-gradient-to-r from-blue-300 to-green-300">
       <div className="text-center">
@@ -15,10 +13,9 @@ const NotFound = () => {
         <p className="text-gray-800 text-xl mt-4">We're sorry, but the page you requested could not be found.</p>
         <p className="text-gray-800 text-xl mt-4 ">It seems that the page you are looking for has vanished into thin air.</p>
         <div className="mt-8">
-          <button
-          type='button' 
-          onClick={()=>goBack()}
-          className="bg-gray-800 text-white py-2 px-4 rounded-lg hover:bg-gray-700">Go Back Home</button>
+         <Link href="/" className="bg-blue-950 text-white text-xl px-4 py-2 rounded-md">
+          Go Back Home
+         </Link>
         </div>
       </div>
     </div>
